20976b62640ea7a22244a8dbe57847e507eac7df
dhiaabdelli12 <dhiaabdelli@outlook.fr>
2025-03-26 00:25:15 +0100

    ingestion into postgres database

--- ingestion/fetch_data.py
+++ ingestion/fetch_data.py
@@ -1,27 +1,43 @@
-import requests
-import pymongo
+import pandas as pd
+from ucimlrepo import fetch_ucirepo
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
 import os
+import logging
 
-# TBD: change data source to timeseries data and simulate streaming
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongo:27017/")
-DB_NAME = "store"
-COLLECTION_NAME = "products"
-API_URL = "https://fakestoreapi.com/products"
+load_dotenv()
 
-def fetch_data():
-    response = requests.get(API_URL)
-    response.raise_for_status()
-    return response.json()
 
-def store_in_mongodb(data):
-    client = pymongo.MongoClient(MONGO_URI)
-    db = client[DB_NAME]
-    collection = db[COLLECTION_NAME]
+logger.info("Fetching dataset from UCI Repository...")
+data = fetch_ucirepo(id=159) 
+X = data.data.features
+y = data.data.targets
 
-    collection.insert_many(data)
-    print(f"Inserted {len(data)} records into MongoDB.")
+logger.info("Combining features and target data into a single DataFrame...")
+combined_df = pd.concat([X, y], axis = 1)
 
-if __name__ == "__main__":
-    data = fetch_data()
-    store_in_mongodb(data)
+db_url = os.getenv('DATABASE_URL')
+
+if not db_url:
+    logger.error("POSTGRES_DB environment variable is not set.")
+    raise ValueError("POSTGRES_DB environment variable is missing.")
+
+logger.info("Creating a connection to the PostgreSQL database...")
+try:
+    engine = create_engine(db_url)
+    logger.info("Database connection established.")
+except Exception as e:
+    logger.error(f"Error connecting to the database: {e}")
+    raise
+
+
+logger.info("Loading data into the PostgreSQL table...")
+try:
+    combined_df.to_sql('magic_gamma_telescope', con=engine, if_exists='replace', index=False)
+    logger.info("Data has been loaded into the PostgreSQL table successfully.")
+except Exception as e:
+    logger.error(f"Error loading data into the database: {e}")
+    raise
